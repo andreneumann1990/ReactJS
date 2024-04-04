599b275aef908c1189fc62ba49185b7604df09ad
AndreNeumann <andreneumann1990@gmail.com>
2024-04-04 09:37:30 +0200

    -

--- example_2_source/next.config.js
+++ example_2_source/next.config.js
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    assetPrefix: '/reactjs/example_2/',
     basePath: '/reactjs/example_2',
     distDir: '../example_2',
     output: 'export',
