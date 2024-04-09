21de7d59c69553c9f054b5280c6e0bdd7af02ff1
AndreNeumann <andreneumann1990@gmail.com>
2024-04-09 20:41:08 +0200

    -

--- src/main/java/com/reactjs/example_3/GreetingController.java
+++ src/main/java/com/reactjs/example_3/GreetingController.java
@@ -4,11 +4,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/reactjs/example_3")
 public class GreetingController {
 
     @GetMapping("/greeting")
